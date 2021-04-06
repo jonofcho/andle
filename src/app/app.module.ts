e257224b9902863c0873d934b218dcbd45ef9949
jonathan cho <jonofcho@gmail.com>
2021-04-05 23:24:39 -0700

    header and product image tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './containers/footer/footer.component';
-import { HeaderComponent } from './containers/header/header.component';
-import { CollectionItemComponent } from './containers/collection-container/components/collection-item/collection-item.component';
-import { HeaderRoutingComponent } from './containers/header-routing/header-routing.component';
-import { CollectionContainerComponent } from './containers/collection-container/collection-container.component';
 import { AboutUsComponent } from './containers/about-us/about-us.component';
-import { CollectionGridComponent } from './containers/collection-container/components/collection-grid/collection-grid.component';
-import { ProductContainerComponent } from './containers/product-container/product-container.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CollectionContainerModule } from './containers/collection-container/collection-container.module';
 import { ProductContainerModule } from './containers/product-container/product-container.module';
 import { ShopifyService } from './services/shopify.service';
-import { Apollo } from 'apollo-angular';
-import { HttpLink } from '@apollo/client/core';
-
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent,
-    HeaderRoutingComponent,
-    AboutUsComponent
+    AboutUsComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,9 +27,16 @@ import { HttpLink } from '@apollo/client/core';
     CollectionContainerModule,
     AppRoutingModule,
     ProductContainerModule,
-    GraphQLModule
+    GraphQLModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    BrowserAnimationsModule,
   ],
   providers: [ShopifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
